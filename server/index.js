fab639c915424504effaacdcbeeaeea3b2305dc1
vibhujay99 <87332777+vibhujay99@users.noreply.github.com>
2022-06-04 12:44:01 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/marking.js';
+import userRoutes from './routes/users.js';
+import subRoutes from './routes/subs.js';
+import tempRoutes from './routes/temps.js';
+import panelRoutes from './routes/panels.js';
+import customerRoutes from './routes/customers.js';
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/marking', postRoutes);
+app.use('/user', userRoutes);
+app.use('/subs', subRoutes);
+app.use('/temps', tempRoutes);
+app.use('/panels', panelRoutes);
+app.use('/customers', customerRoutes);
+
+//const CONNECTION_URL = 'mongodb+srv://vibhujay99:codelyoko99@cluster0.zibrl.mongodb.net/Cluster0?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true} )
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+
